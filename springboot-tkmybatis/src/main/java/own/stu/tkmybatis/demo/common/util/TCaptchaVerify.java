c564ea4e4c2c20374d132e801a2247ac1c8ec63b
liang.chen <liang.chen@lianshang.com>
2018-11-11 11:41:14 +0800

    fix-not-success

--- springboot-tkmybatis/src/main/java/own/stu/tkmybatis/demo/common/util/TCaptchaVerify.java
+++ springboot-tkmybatis/src/main/java/own/stu/tkmybatis/demo/common/util/TCaptchaVerify.java
@@ -3,62 +3,80 @@ package own.stu.tkmybatis.demo.common.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class TCaptchaVerify {
     private static final String APP_ID = "2055559515";
     private static final String APP_SECRET = "0oUjfuoqM7P56tXw-BV7YQA**";
-    private static final String VERIFY_URI = "https://ssl.captcha.qq.com/ticket/verify?aid=%s&AppSecretKey=%s&Ticket=%s&Randstr=%s&UserIP=%s";
+    private static final String VERIFY_URI = "https://ssl.captcha.qq.com/ticket/verify";
+    private static final String VERIFY_URI_WITH_PARAM = "https://ssl.captcha.qq.com/ticket/verify?aid=%s&AppSecretKey=%s&Ticket=%s&Randstr=%s&UserIP=%s";
 
     public static int verifyTicket(String ticket, String rand, String userIp) {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet;
-        CloseableHttpResponse response = null;
-        try {
-            httpGet = new HttpGet(String.format(VERIFY_URI,
-                    APP_ID,
-                    APP_SECRET,
-                    URLEncoder.encode(ticket, "UTF-8"),
-                    URLEncoder.encode(rand, "UTF-8"),
-                    URLEncoder.encode(userIp, "UTF-8")
-            ));
-            response = httpclient.execute(httpGet);
 
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                String res = EntityUtils.toString(entity);
-                 System.out.println(res); // 临时输出
-
-                JSONObject result = JSON.parseObject(res);
-                // 返回码
-                int code = result.getInteger("response");
-                // 恶意等级
-                int evilLevel = result.getInteger("evil_level");
-
-                // 验证成功
-              if (code == 1) {
-//                return evilLevel;
-                return code;
-              }
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> params = new HashMap<>();
+        params.put("aid", APP_ID);
+        params.put("AppSecretKey", APP_SECRET);
+        params.put("Ticket", ticket);
+        params.put("Randstr", rand);
+        params.put("UserIP", userIp);
+        ResponseEntity<WaterWallResponseEntity> responseEntity =
+            restTemplate.getForEntity(VERIFY_URI, WaterWallResponseEntity.class, params);
+        if(responseEntity.getStatusCode().equals(HttpStatus.OK)){
+            WaterWallResponseEntity data = responseEntity.getBody();
+            if(data.getResponse() == 1){
+                return 1;
+            }else {
+                System.out.println(data.getErr_msg());
+                return -1;
             }
-        } catch (java.io.IOException e) {
-            // 忽略
-        } finally {
-            try {
-                response.close();
-            } catch (Exception ignore) {
-            }
+        }else{
+            System.out.println("服务器连接失败");
+            return -1;
         }
-
-        return -1;
     }
 
     public static void main(String[] args) throws Exception {
         verifyTicket("112", "111", "127.0.0.1");
+    }
+
+    static class WaterWallResponseEntity{
+        private int response;
+        private int evil_level;
+        private String err_msg;
+
+        public int getResponse() {
+            return response;
+        }
+
+        public void setResponse(int response) {
+            this.response = response;
+        }
+
+        public int getEvil_level() {
+            return evil_level;
+        }
+
+        public void setEvil_level(int evil_level) {
+            this.evil_level = evil_level;
+        }
+
+        public String getErr_msg() {
+            return err_msg;
+        }
+
+        public void setErr_msg(String err_msg) {
+            this.err_msg = err_msg;
+        }
     }
 }
