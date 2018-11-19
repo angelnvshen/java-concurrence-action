059c9223c7394fb761b531015cc29a27a9bb7837
liang.chen <liang.chen@lianshang.com>
2018-11-19 18:57:40 +0800

    add async test

--- springboot-tkmybatis/src/main/java/own/stu/tkmybatis/demo/DemoApplication.java
+++ springboot-tkmybatis/src/main/java/own/stu/tkmybatis/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package own.stu.tkmybatis.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(basePackages = "own.stu.tkmybatis.demo.dao")
 @SpringBootApplication
+@EnableAsync
 public class DemoApplication {
 
 	public static void main(String[] args) {
