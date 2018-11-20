f4f5c030d5167daa210333aa41e49a150e083b59
liang.chen <liang.chen@lianshang.com>
2018-11-20 19:25:19 +0800

    fix

--- redis/src/main/java/own/stu/com/redis/controller/RedisController.java
+++ redis/src/main/java/own/stu/com/redis/controller/RedisController.java
@@ -1,10 +1,12 @@
 package own.stu.com.redis.controller;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import own.stu.com.redis.cache.service.IRedisCacheService;
+import own.stu.com.redis.lock.RedisLock;
 import own.stu.com.redis.model.User;
 
 @RequestMapping("redis")
@@ -23,4 +25,21 @@ public class RedisController {
     redisCacheService.set(user.getUsername(), user.getPassword());
     return "success";
   }
+
+  @RequestMapping("lock")
+  public String lock(User user) {
+    if (user == null) {
+      return "";
+    }
+    RedisLock lock = new RedisLock("test", "redis");
+    try {
+      lock.acquire(1, TimeUnit.MINUTES);
+      redisCacheService.set(user.getUsername(), user.getPassword());
+      return "success";
+    }finally {
+      lock.release();
+    }
+  }
+
+
 }
