bd6e60a9ae41140a9444753421b87534e9f494a5
liang.chen <liang.chen@lianshang.com>
2019-01-30 10:56:40 +0800

    lambda forkAndJoin

--- source-core/src/test/java/own/stu/sourcecore/junit/PureTest.java
+++ source-core/src/test/java/own/stu/sourcecore/junit/PureTest.java
@@ -1,8 +1,23 @@
 package own.stu.sourcecore.junit;
 
+import java.util.HashMap;
+import java.util.Map;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONUtil;
+import org.assertj.core.util.Lists;
+
 public class PureTest {
 
   public static void main(String[] args) {
-    junit.textui.TestRunner.run(CalculationTest.class);
+//    junit.textui.TestRunner.run(CalculationTest.class);
+
+    Map map = new HashMap();
+    map.put("12", "32232");
+    map.put("we", "32232");
+    map.put("223", Lists.newArrayList("2", "3"));
+    map.put("44", Lists.newArrayList());
+    System.out.println(JSONObject.toJSONString(map));
+
+    System.out.println("310111195410231232".length());
   }
 }
