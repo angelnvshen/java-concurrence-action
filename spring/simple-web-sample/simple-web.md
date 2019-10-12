06091cc86de7957d6517657581c9f55e2681c7f8
my <angelnvshen@163.com>
2019-10-12 20:53:55 +0800

    support with annotation on servlet 3.0

---
+++ spring/simple-web-sample/simple-web.md
@@ -0,0 +1,12 @@
+### simple web project 
+
+tip:with maven, without spring 
+
+#### 1：简单的servelt类和对 webapp 下静态资源的访问
+
+#### 2：servlet3.0 之后对 注解的支持， @WebServlet @WebFilter @WebListener 
+
+
+
+
+
---
+++ spring/simple-web-sample/src/main/java/own/stu/web/annotation/AnnotationServlet.java
@@ -0,0 +1,18 @@
+package own.stu.web.annotation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/annotation-servlet", "/second-servlet"})
+public class AnnotationServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>Sample Annotation Servlet</h1>");
+    }
+}
