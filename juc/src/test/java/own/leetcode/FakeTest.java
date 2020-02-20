f033493c86584862519292bd8072c4156bb2da93
my <angelnvshen@163.com>
2020-02-20 21:55:54 +0800

    leetcode 刷题

--- juc/src/test/java/own/leetcode/FakeTest.java
+++ juc/src/test/java/own/leetcode/FakeTest.java
@@ -508,14 +508,6 @@ public class FakeTest {
         return nums;
     }
 
-
-    @Test
-    public void test1() {
-//        System.out.println(maxProfit(new int[]{4, 4, 6, 1, 1, 4, 2, 5}));
-        System.out.println(maxProfit(new int[]{2,1}));
-    }
-
-
     public int maxProfit(int[] prices) {
         // write your code here
         if (prices == null || prices.length == 0) {
@@ -556,4 +548,338 @@ public class FakeTest {
         return ans;
     }
 
+//    int binarySearch()
+
+    public int lengthOfLIS_i(int[] nums) {
+        int result = 0;
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] pi = new int[n];// 其中的一个可能路径的下标
+        int p = -1;//pi数组中的可能路径的最后一个下标
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            pi[i] = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (dp[j] + 1 == dp[i]) {
+                        pi[i] = j;
+                    }
+                }
+            }
+
+            // result
+            result = Math.max(result, dp[i]);
+            if (dp[i] == result) {
+                p = i;
+            }
+        }
+
+        System.out.println(p);
+        int[] seq = new int[result];
+        for (int i = result - 1; i >= 0; i--) {
+            seq[i] = nums[p];
+            p = pi[p];
+        }
+
+
+        printArray(nums);
+        printArray(dp);
+        printArray(pi);
+        printArray(seq);
+        return result;
+    }
+
+    private void printArray(int[] arrays) {
+        for (int i : arrays) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        return 0;
+    }
+
+
+    public int numSquares(int n) {
+
+        if (n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    public String longestPalindrome(String s) {
+        // write your code here
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        int n = s.length(), j = 1, k = 1, L = 0, R = 0, max = 0;
+        ;
+        //以字符为中心扩展 类似 aba
+        for (int i = 0; i <= n - 1; i++) {
+            j = i;
+            k = i;
+            while (j >= 0 && k < n) {
+                if (s.charAt(j) != s.charAt(k)) {
+                    break;
+                }
+                if (max < k - j) {
+                    R = k;
+                    L = j;
+                    max = k - j;
+                }
+                j--;
+                k++;
+            }
+        }
+        //以空格为中心扩展 类似 abba
+        for (int i = 0; i < n - 1; i++) {
+            j = i;
+            k = i + 1;
+            while (j >= 0 && k < n) {
+                if (s.charAt(j) != s.charAt(k)) {
+                    break;
+                }
+                if (max < k - j) {
+                    R = k;
+                    L = j;
+                    max = k - j;
+                }
+                j--;
+                k++;
+            }
+        }
+        System.out.println(max + ", " + L + ", " + R);
+        return s.substring(L, R + 1);
+    }
+
+    public int minCut(String s) {
+        // 最小分割次数，划分型动态规划
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int n = s.length(), ans = n;
+        char[] chars = s.toCharArray();
+        int[] dp = new int[n + 1];
+        // dp[i] = Math.min(dp[j] + 1); j in 0 ~ i -1 且 j~i 是回文串
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = n;//Integer.MAX_VALUE;
+            for (int j = i - 1; j >= 0; j--) {
+                if (isPalin(chars, j, i - 1)) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n] - 1;
+    }
+
+    private boolean isPalin(char[] chars, int i, int j) {
+        if (i > j) return false;
+
+        while (i < j) {
+            if (chars[i] != chars[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public int copyBooks(int[] pages, int k) {
+        // write your code here
+        if (pages == null || pages.length == 0 || k <= 0) {
+            return 0;
+        }
+
+        int n = pages.length, ans = 0;
+        if (n <= k) {
+            for (int p : pages) {
+                ans = Math.max(ans, p);
+            }
+            return ans;
+        }
+        if (n < k) k = n - 1;
+        /*
+        k个人复印n本书，相当于将数组分为k段，求每段的和的最大值的最小
+        dp[i] =
+        */
+        int[][] dp = new int[k + 1][n + 1];
+        dp[0][0] = 0;//0个人0本书
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = Integer.MAX_VALUE;//0个人i本书
+        }
+
+        for (int i = 1; i <= k; i++) {
+            dp[i][0] = 0;//k个人处理0本书
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                int sum = 0;
+                // dp[k][i] = min j in 0 ...i ( max(dp[k - 1][j], A[j] +..+ A[i])
+                for (int K = j; K >= 0; K--) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][K], sum));
+
+                    if (K > 0) {
+                        sum += pages[K - 1];
+                    }
+                }
+            }
+        }
+        return dp[k][n];
+    }
+
+    public int backPackV(int[] nums, int target) {
+        // write your code here
+        if (nums == null || nums.length == 0 || target <= 0) {
+            return 0;
+        }
+
+        int n = nums.length, m = target;
+        int[][] dp = new int[n + 1][m + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - nums[i - 1] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        char[] chars = s.toCharArray();
+        int n = s.length();
+        int l, r, len = 0;
+        int[][] dp = new int[n][n];
+        // len  = 1; <=>r -l =0,即开始位置和结束位置的间隔长度
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;// 单个字符串的回文序列就是自己，长度为1；
+        }
+        // len  = 2;
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i + 1] = chars[i] == chars[i + 1] ? 2 : 1;
+        }
+        // 0, 1, 2 ... n - 2, n - 1
+        for (len = 3; len <= n; len++) {
+            for (l = 0; l <= n - len; l++) {
+                r = l + len - 1;
+                // 去掉左边或者去掉右边
+                dp[l][r] = Math.max(dp[l + 1][r], dp[l][r - 1]);
+                if (chars[r] == chars[l]) {
+                    dp[l][r] = Math.max(dp[l][r], dp[l + 1][r - 1] + 2);
+                }
+            }
+        }
+        printArray(dp);
+        return dp[0][n - 1]; //l 和 r 长度间隔为len
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        printArray(nums);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(findDuplicate(new int[]{5, 5, 4, 3, 2, 1}));
+    }
+
+    public int findDuplicate(int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) return 0;
+
+        int l = 0, r = nums.length - 1, mid = -1;
+        while (l + 1 < r) {
+            mid = l + (r - l) / 2;
+            if (leftSide(mid, nums)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+
+        if (leftSide(l, nums)) return l;
+        return r;
+    }
+
+    private boolean leftSide(int target, int[] nums) {
+        int count = 0;
+        for (int i : nums) {
+            if (i <= target) count += 1;
+        }
+
+        return count > target;
+    }
+
+    public void sortIntegers(int[] A) {
+        // write your code here
+        if(A == null || A.length == 0) return;
+
+        // quick-sort
+        quickSort(A, 0, A.length -1);
+    }
+
+    private void quickSort(int[] A, int start, int end){
+        if(start >= end) return;
+
+        int left = start, right = end;
+        int pivot = A[(start + end) / 2];
+
+        while(left <= right){
+            while(left <= right && A[left] < pivot){
+                left ++;
+            }
+            while(left <= right && A[right] > pivot){
+                right --;
+            }
+            if(left <= right){
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left ++;
+                right --;
+            }
+        }
+
+        quickSort(A, start, right);
+        quickSort(A, left, end);
+    }
 }
